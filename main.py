1a588bd8afa0ea31f21086630779b4b9de4c5ed4
Jberman0 <j.berman105@gmail.com>
2025-06-29 03:11:29 +0000

    maybe this time

--- main.py
+++ main.py
@@ -352,7 +352,7 @@ def process_master_file_update(session, data, entries, questionnaire, folder_id,
         return False
 
 def merge_csvs_for_participant(session, folder_id, study_type, source, participant_id, date_str, entries, 
-                        QUESTIONNAIRE_ORDER=None, questionnaire=None, group_row, question_row, data_row):
+                        group_row, question_row, data_row, QUESTIONNAIRE_ORDER=None, questionnaire=None):
     """
     Horizontally merge all questionnaire CSVs for a participant/session (same date) into one CSV.
     - Each file has two header rows and one data row.
