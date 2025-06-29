83cdb0cddeb70086536cb69ff5bb82741340bc7b
Jberman0 <j.berman105@gmail.com>
2025-06-29 21:23:52 +0000

    yay

--- main.py
+++ main.py
@@ -467,7 +467,7 @@ def merge_csvs_for_participant(session, folder_id, study_type, source, participa
     # Get or create the 'single_file' subfolder for merged files 
     single_file_folder_id = get_or_create_subfolder(session, folder_id, subfolder_name="single_file")
     if upload_file(session, merged_filename, buf.getvalue(), single_file_folder_id):
-        print(f"Horizontally merged CSV uploaded as {merged_filename} to 'single_file' subfolder")
+        print(f"Horizontally merged CSV uploaded as {merged_filename} to '{single_file}' subfolder")
         return True
     else:
         return False
